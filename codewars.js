6f3b5c733b62b6a2fa928dd177fecb8c2a87450e
Vitalino78 <vitalinos1978@gmail.com>
2021-02-25 00:13:01 +0200

    4

--- codewars.js
+++ codewars.js
@@ -123,3 +123,54 @@
 //   return result_arr.join(' ');
 
 // }
+
+// You probably know the "like" system from Facebook and other pages.
+// People can "like" blog posts, pictures or other items.
+// We want to create the text that should be displayed next to such an item.
+
+// Implement a function likes :: [String] -> String, which must take in input array,
+// containing the names of people who like an item.It must return the display text as shown in the examples:
+
+// likes [] -- must be "no one likes this"
+// likes ["Peter"] -- must be "Peter likes this"
+// likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others
+
+// function likes(names) {
+
+//   let i = 0;
+
+//   const len = names.length;
+
+//   if(len == 0) {
+
+//     return 'no one likes this';
+
+//   }
+
+//   if (len == 1) {
+
+//     return `${names[i]} likes this`;
+
+//   }
+
+//   if (len == 2) {
+
+//     return `${names[i]} and ${names[i+1]} like this`;
+
+//   }
+
+//   if (len == 3){
+
+//     return `${names[i]}, ${names[i+1]} and ${names[i+2]} like this`
+
+//   }
+
+//   if (len >= 4){
+
+//     return `${names[i]}, ${names[i+1]} and ${len-2} others like this`
+
+//   }
+
+// }
