66374d7d57aa25c866ffd868ca5f74b7fd57d542
Vitalino78 <vitalinos1978@gmail.com>
2021-02-25 22:23:14 +0200

    desision

--- codewars.js
+++ codewars.js
@@ -207,24 +207,57 @@
 // isIsogram("aba") == false
 // isIsogram("moOse") == false // -- ignore letter case
 
-You are going to be given a word. 
-Your job is to return the middle character of the word. 
-If the word's length is odd, return the middle character. If the word's length is even, 
-return the middle 2 characters.
+// You are going to be given a word. 
+// Your job is to return the middle character of the word. 
+// If the word's length is odd, return the middle character. If the word's length is even, 
+// return the middle 2 characters.
 
-#Examples:
+// #Examples:
 
-Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("test") should return "es"
 
-Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("testing") should return "t"
 
-Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("middle") should return "dd"
 
-Kata.getMiddle("A") should return "A"
-#Input
+// Kata.getMiddle("A") should return "A"
+// #Input
 
-A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more 
+//   than 1000 in some test cases due to an error in the test cases). 
+//   You do not need to test for this. This is only here to tell you that you do not need to worry 
+//   about your solution timing out.
 
-#Output
+//   function getMiddle(s)
 
-The middle character(s) of the word represented as a string.
+// {
+
+//   const word = s.split('')
+
+//   const letters = [];
+
+//   for (let letter of word) {
+
+//    letters.push(letter);
+
+//   }
+
+//   //return letters;
+
+//   if (letters.length % 2 !== 0) {
+
+//     return letter.indexOf(letters.length % 2 !== 0)
+
+//   }
+
+//   if (letters.length % 2 === 0) {
+
+//     return letter.indexOf((letters.length % 2 === 0) - 1 && (letters.length % 2 === 0) + 1))
+
+//   }
+
+// }
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
