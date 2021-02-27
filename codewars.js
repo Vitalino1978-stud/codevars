51123fd08cffe6f388ea6c7a3034cc797a8f003e
Vitalino78 <vitalinos1978@gmail.com>
2021-02-27 04:48:56 +0200

    musk numbers

--- codewars.js
+++ codewars.js
@@ -261,3 +261,30 @@
 // #Output
 
 // The middle character(s) of the word represented as a string.
+
+// Usually when you buy something, you're asked whether your credit card number, 
+// phone number or answer to your most secret question is still correct.
+// However, since someone could look over your shoulder, you don't want that shown on your screen. 
+// Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// // return masked string
+
+// function maskify(cc) {
+
+// const symb = cc.split('');
+
+//   //const mask = symb.splice(0, symb.length-4, '#' )
+
+//   for (let i = 0; i < symb.length-4; i++) {
+
+//     symb[i] = '#';
+
+//   }
+
+//   const masked = symb.join('')
+
+// return masked;
+
+// }
