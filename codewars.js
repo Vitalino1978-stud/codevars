8aafb3c947bbfcb3737bcff8cc81a375ee2276cb
Vitalino78 <vitalinos1978@gmail.com>
2021-02-27 04:51:45 +0200

    finding divisors

--- codewars.js
+++ codewars.js
@@ -288,3 +288,34 @@
 // return masked;
 
 // }
+
+// Create a function named divisors / Divisors that takes an integer n > 1 and returns an array
+// with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. 
+// If the number is prime return the string '(integer) is prime'(null in C#)
+//   (use Either String a in Haskell and Result < Vec < u32 >, String > in Rust).
+
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+// function divisors(integer) {
+
+//   let divisor = 2;
+
+//   const res = [], max_divisor = Math.floor(integer/2);
+
+//   while (divisor <= max_divisor) {
+
+//     if (integer % divisor === 0) {
+
+//       res.push(divisor);
+
+//     }
+
+//     divisor++;
+
+//   }
+
+//   return (res.length>0?res:`${integer} is prime`);
+
+// };
