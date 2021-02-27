e79bfb1a1094167aaa61c1ac73de2aed56de1703
Vitalino78 <vitalinos1978@gmail.com>
2021-02-27 04:54:37 +0200

    squre every digit

--- codewars.js
+++ codewars.js
@@ -319,3 +319,25 @@
 //   return (res.length>0?res:`${integer} is prime`);
 
 // };
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+// function squareDigits(num){
+
+//     const string = num.toString();
+
+//     const results = [];
+
+//     for (let i = 0; i < string.length; i++){
+
+//         results[i] = string[i] * string[i];
+
+//     }
+
+//     return Number(results.join(''));
+
+// };
