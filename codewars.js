3404f8907d05e2c47c9fab6889562ca8c7ace460
Vitalino78 <vitalinos1978@gmail.com>
2021-02-27 04:58:35 +0200

    pin length and valid data

--- codewars.js
+++ codewars.js
@@ -341,3 +341,50 @@
 //     return Number(results.join(''));
 
 // };
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything
+// but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -- > false
+
+// function validatePIN (pin) {
+
+
+//   if (pin.length == 4 || pin.length == 6) {
+
+//     const arr = pin.split('');
+
+//     let res=true;
+
+//     for (item of arr) 
+
+//       if (!(item>='0' && item<='9'))  {
+
+//         res=false;
+
+//         break;
+
+//       }
+
+//     return res;
+
+//   }  else
+
+//       return false;
+
+ 
+
+//   //return true or false
+
+// }
+
+ 
+
+
+
+
