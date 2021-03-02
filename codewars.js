0dfb4161e19a01debc2468a5cafd256ea3638a1c
Vitalino78 <vitalinos1978@gmail.com>
2021-03-02 21:41:53 +0200

    appearance of integer in array

--- codewars.js
+++ codewars.js
@@ -411,6 +411,53 @@
 
   // }
  
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+  
+//   function findOdd(A) {
+
+//   const statistic = [];
+
+//   for (let i = 0; i < A.length; i++){
+
+//     let elem = null;
+
+//     if (elem = statistic.find(item => item.number == A[i]))  {
+
+//       elem.count++;
+
+//     }   else {
+
+//       statistic.push({'number':A[i], 'count':1});
+
+//     }
+
+//   }
+
+ 
+
+//   for (stat of statistic)  {
+
+//     if (stat.count%2 != 0)  {
+
+//       return stat.number;
+
+//     }
+
+//   }
+
+//   //happy coding!
+
+//   return null;
+
+// }
+
+// doTest([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5], 5);
+//   doTest([1,1,2,-2,5,2,4,4,-1,-2,5], -1);
+//   doTest([20,1,1,2,2,3,3,5,5,4,20,4,5], 5);
+//   doTest([10], 10);
+//   doTest([1,1,1,1,1,1,10,1,1,1,1], 10);
 
 
 
