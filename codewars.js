24eaf098ca3add57f594ff352574071c5eb496b0
Vitalino78 <vitalinos1978@gmail.com>
2021-03-12 23:46:15 +0200

    sum of two minimum elems

--- codewars.js
+++ codewars.js
@@ -533,5 +533,24 @@
 
 // }
 
+// Create a function that returns the sum of the two lowest positive numbers given an array of
+// minimum 4 positive integers.No floats or non - positive integers will be passed.
+
+// For example, when an array is passed like[19, 5, 42, 2, 77], the output should be 7.
+
+// function sumTwoSmallestNumbers(numbers) { 
+
+// numbers.sort(function(a, b) {
+
+//   return a - b;
+
+// });
+
+//   let sum = numbers[0] + numbers[1];
+
+//   return sum;
+
+// }
+
 
 
