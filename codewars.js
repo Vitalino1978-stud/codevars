bf6d83504e09c768a7f2d3ffac0bd1183b774b86
Vitalino78 <vitalinos1978@gmail.com>
2021-03-12 23:53:39 +0200

    the highest scoring word

--- codewars.js
+++ codewars.js
@@ -578,4 +578,70 @@
  
 
 // }
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+// function high(x){
+
+//   let words = x.split(' ' ),
+
+//       index_max_score=-1,
+
+//       max_score = 0;
+
+ 
+
+//   var scoreWords = words.map(function(word, ind) {
+
+//     let score=0;
+
+//     for (let i=0; i<word.length; i++)
+
+//       score += word.charCodeAt(i)-96;
+
+   
+
+//     if (max_score < score)  {
+
+//       max_score = score;
+
+//       index_max_score = ind;
+
+//     }
+
+//     return score;
+
+//   });
+
+ 
+
+//   return words[index_max_score];
+
+// }
+
+ 
+
+// "c".charCodeAt(0)-96
+
+ 
+
+
+
+
+ 
+
+ 
+
+
+  
+ 
+
+
 
