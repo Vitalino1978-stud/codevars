20d0d02b2726b8d2d3951de3f2e16967ed1a4d02
Vitalino78 <vitalinos1978@gmail.com>
2021-03-12 23:49:17 +0200

    creation of phone number

--- codewars.js
+++ codewars.js
@@ -552,5 +552,30 @@
 
 // }
 
+// Write a function that accepts an array of 10 integers(between 0 and 9),
+// that returns a string of those numbers in the form of a phone number.
 
+// Example:
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890
+// function createPhoneNumber(numbers){
+
+// //   const spliced = numbers.splice(0, 0, '(');
+
+// //   const spliced1 = spliced.splice(4, 0, ')');
+
+// //   const spliced2 = spliced1.splice(7, 0, '-');
+
+ 
+
+//   numbers.splice(0, 0, '(');
+
+//   numbers.splice(4, 0, ') ');
+
+//   numbers.splice(8, 0, '-');
+
+//   return numbers.join('');
+
+ 
+
+// }
 
